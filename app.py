dd486fee2d15ac40494875ba213a732823e821ca
Shuvam Sardar <shuvamsardar2002@gmail.com>
2024-12-12 18:51:30 +0530

    Disable File Upload for Complexity

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import plotly.express as px
 
 st.set_page_config(page_title="Sentiment Analysis Web App", page_icon="🦜", layout="centered", initial_sidebar_state="collapsed")
 
-# Load the pre-trained model and tokenizer
 @st.cache_resource
 def load_model():
     model = TFBertForSequenceClassification.from_pretrained('model/')
@@ -16,48 +15,51 @@ def load_model():
 
 model, tokenizer = load_model()
 
-# Function to predict sentiment and score
+def plot_sentiment_distribution(reviews_df):
+    sentiment_count = reviews_df['Sentiment'].value_counts().reset_index()
+    sentiment_count.columns = ['Sentiment', 'Count']
+    fig = px.bar(sentiment_count, x='Sentiment', y='Count', color='Sentiment', title="Sentiment Distribution of Reviews")
+    st.plotly_chart(fig)
+
+
 def predict_sentiment(review):
     inputs = tokenizer(review, return_tensors="tf", padding=True, truncation=True, max_length=128)
     logits = model(inputs['input_ids'], attention_mask=inputs['attention_mask']).logits
-    predicted_class = tf.argmax(logits, axis=1).numpy()[0]
-    
-    # Calculate sentiment score
     score = tf.nn.softmax(logits).numpy()[0]
-    
-    return "Positive" if predicted_class == 1 else "Negative", score
+    if score[1] > 0.65:
+        sentiment = "Positive"
+    elif score[0] > 0.65:
+        sentiment = "Negative"
+    else:
+        sentiment = "Neutral"
+    return sentiment, score
 
-# Initialize session state to store results if not already initialized
+def shorten_text(text, max_len=10):
+    if len(text) > max_len:
+        return text[:max_len] + '...'
+    return text
+
 if 'results' not in st.session_state:
     st.session_state.results = []
 
 if 'comparison_results' not in st.session_state:
     st.session_state.comparison_results = []
 
-# Custom CSS
 st.markdown("""
     <style>
-    @import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Pacifico&family=Volkhov:ital,wght@0,400;0,700;1,400;1,700&display=swap');
-            
-            
-            
     .st-markdown-container {
         background-color: #252A34;
         color: #FFFFFF;
         border-radius: 10px;
-        padding: 2rem;
         font-size: 1.2rem;
     }
-
     .stMarkdown p {
         font-size: 1.2rem;
     }
-
     .review-label, .compare-label, .upload-label {
         font-size: 1.2rem;
         color: #FFFFFF;
     }
-
     .stTextArea textarea {
         width: 100%;
         height: 170px;
@@ -65,70 +67,54 @@ st.markdown("""
         padding: 10px;
         font-size: 16px;
     }
-
     .main-title {
         font-size: 2.5rem;
-        color: #FFD700;
-        font-family: "Pacifico", cursive;
-        font-weight: 200;
-        font-style: normal;
+        color:  #16f2b3;
     }
-
     .section-title {
         font-size: 1.5rem;
         color: #87CEFA;
     }
-
     .score-positive {
         color: #2EB086;
     }
-
     .score-negative {
         color: #de3f04;
     }
+    .score-neutral {
+        color: #FFDD57;
+    }
     .st-emotion-cache-1qg05tj, .st-emotion-cache-uef7qa { 
-            display: none;
+        display: none;
     }
     </style>
     """, unsafe_allow_html=True)
 
-# Streamlit app layout
-st.markdown("<h1 class='main-title'>✨ Discover the Sentiment Behind Text</h1>", unsafe_allow_html=True)
-st.write("Enter a product review to find out if the sentiment is positive or negative:")
+st.markdown("<h1 class='main-title'>Discover the Sentiment Behind Text</h1>", unsafe_allow_html=True)
+st.write("Enter a product review to find out if the sentiment is positive, negative, or neutral:")
 
-# User input for single review
 st.markdown("<label class='review-label'>✍️ Review Text</label>", unsafe_allow_html=True)
 user_review = st.text_area("", "", placeholder="Type your review here...")
 
-# Predict sentiment button
 if st.button("Predict Sentiment"):
     if user_review:
-        sentiment, score = predict_sentiment(user_review)
-        
-        # Convert scores to percentages
-        score_percentage = score * 100
-
-        # Display sentiment result
-        if sentiment == "Positive":
-            st.success(f"The sentiment of the review is: **{sentiment}** 😊", icon="✅")
-        else:
-            st.error(f"The sentiment of the review is: **{sentiment}** 😟", icon="❌")
-
-        # Display sentiment score in one line
-        st.markdown(f"<h3 class='section-title'>Sentiment Score: &nbsp &nbsp<span class='score-positive'>Positive: {score_percentage[1]:.2f}%</span> &nbsp &nbsp <span class='score-negative'>Negative: {score_percentage[0]:.2f}%</span></h3>", unsafe_allow_html=True)
-
-        # Breakdown of sentiment components
-        breakdown = pd.DataFrame({"Sentiment": ["Positive", "Negative"], "Percentage": score_percentage})
-        fig = px.bar(breakdown, x='Sentiment', y='Percentage', color='Sentiment', title="Sentiment Breakdown")
-        st.plotly_chart(fig)
-
-        # Save result for export
-        st.session_state.results.append({"Review": user_review, "Sentiment": sentiment, "Positive Score": score_percentage[1], "Negative Score": score_percentage[0]})
-
+        with st.spinner('Analyzing...'):
+            sentiment, score = predict_sentiment(user_review)
+            score_percentage = score * 100
+            if sentiment == "Positive":
+                st.success(f"The sentiment of the review is: **{sentiment}** 😊", icon="✅")
+            elif sentiment == "Negative":
+                st.error(f"The sentiment of the review is: **{sentiment}** 😟", icon="❌")
+            else:
+                st.info(f"The sentiment of the review is: **{sentiment}** 😐", icon="ℹ️")
+            st.markdown(f"<h3 class='section-title'>Sentiment Score: &nbsp;<span class='score-positive'>Positive: {score_percentage[1]:.2f}%</span> &nbsp;<span class='score-negative'>Negative: {score_percentage[0]:.2f}%</span></h3>", unsafe_allow_html=True)
+            breakdown = pd.DataFrame({"Sentiment": ["Positive", "Negative"], "Percentage": score_percentage})
+            fig = px.bar(breakdown, x='Sentiment', y='Percentage', color='Sentiment', title="Sentiment Breakdown")
+            st.plotly_chart(fig)
+            st.session_state.results.append({"Review": user_review, "Sentiment": sentiment, "Positive Score": score_percentage[1], "Negative Score": score_percentage[0]})
     else:
         st.warning("Please enter a review to analyze.")
 
-# Comparison feature in main content section
 st.markdown("<h2 class='section-title'>🔍 Compare Multiple Reviews</h2>", unsafe_allow_html=True)
 st.markdown("<label class='compare-label'>Enter multiple reviews (comma-separated)</label>", unsafe_allow_html=True)
 compare_reviews = st.text_area("", "", key="compare_reviews", placeholder="Review 1, Review 2, ...")
@@ -138,60 +124,97 @@ if st.button("Compare Sentiments", key="compare_button"):
         reviews = [rev.strip() for rev in compare_reviews.split(",")]
         comparison_results = []
         for rev in reviews:
-            sentiment, score = predict_sentiment(rev)
-            
-            # Convert scores to percentages
-            score_percentage = score * 100
-            
-            comparison_results.append({"Review": rev, "Sentiment": sentiment, "Positive Score": score_percentage[1], "Negative Score": score_percentage[0]})
-
+            with st.spinner(f'Analyzing review: {rev}'):
+                sentiment, score = predict_sentiment(rev)
+                score_percentage = score * 100
+                comparison_results.append({"Review": rev, "Sentiment": sentiment, "Positive Score": f"{score_percentage[1]:.2f}%", "Negative Score": f"{score_percentage[0]:.2f}%"})
         comparison_df = pd.DataFrame(comparison_results)
         st.write(comparison_df)
-
-        # Visualization for comparison results
-        fig = px.bar(comparison_df, x='Review', y=['Positive Score', 'Negative Score'], barmode='group', title="Sentiment Comparison")
+        comparison_df_melted = comparison_df.melt(id_vars=['Review', 'Sentiment'], value_vars=['Positive Score', 'Negative Score'], var_name='Type', value_name='Percentage')
+        comparison_df_melted['Percentage'] = comparison_df_melted['Percentage'].str.rstrip('%').astype('float')
+        comparison_df_melted['Review'] = comparison_df_melted['Review'].apply(shorten_text)
+        fig = px.bar(comparison_df_melted, x='Review', y='Percentage', color='Type', barmode='group', title="Sentiment Comparison")
+        
+        # Rotate and shorten the axis text for better readability
+        fig.update_layout(
+            xaxis=dict(
+                tickangle=0 # Rotate the labels 45 degrees
+            )
+        )
         st.plotly_chart(fig)
-
-        # Save comparison results to session state for export
         st.session_state.comparison_results = comparison_results
-
-# File upload for bulk review analysis in main content section
-st.markdown("<h2 class='section-title'>📂 Upload Reviews for Analysis</h2>", unsafe_allow_html=True)
-st.markdown("<label class='upload-label'>Choose a CSV file</label>", unsafe_allow_html=True)
-uploaded_file = st.file_uploader("", type="csv")
-if uploaded_file:
-    df = pd.read_csv(uploaded_file)
-    st.write("Uploaded Reviews:")
-    st.write(df.head())
-    
-    if st.button("Analyze Uploaded Reviews"):
-        review_texts = df['Review'].tolist()
-        results = []
-        for review in review_texts:
-            sentiment, score = predict_sentiment(review)
-            
-            # Convert scores to percentages
-            score_percentage = score * 100
-            
-            results.append({"Review": review, "Sentiment": sentiment, "Positive Score": score_percentage[1], "Negative Score": score_percentage[0]})
+        # Plot sentiment distribution
+        plot_sentiment_distribution(comparison_df)
         
-        result_df = pd.DataFrame(results)
-        st.write("Sentiment Analysis Results:")
-        st.write(result_df)
-        
-        # Visualization of sentiment distribution
-        sentiment_counts = result_df['Sentiment'].value_counts().reset_index()
-        sentiment_counts.columns = ['Sentiment', 'Count']
-        fig = px.bar(sentiment_counts, x='Sentiment', y='Count', color='Sentiment', title="Sentiment Distribution")
-        st.plotly_chart(fig)
 
-# About section in the sidebar
-st.sidebar.header("About This App 📖")
+# # Section for uploading CSV and analyzing reviews
+# st.markdown("<h2 class='section-title'>📂 Upload Reviews for Analysis</h2>", unsafe_allow_html=True)
+# st.markdown("<label class='upload-label'>Choose a CSV file</label>", unsafe_allow_html=True)
+# uploaded_file = st.file_uploader("", type="csv")
+
+# if uploaded_file:
+#     df = pd.read_csv(uploaded_file)
+#     st.write("Uploaded Reviews:")
+#     st.write(df.head())
+
+#     if 'Review' not in df.columns:
+#         st.error("The CSV file does not contain a 'Review' column. Please upload a file with a 'Review' column containing the text data you want to analyze.")
+#     else:
+#         if st.button("Analyze Uploaded Reviews"):
+#             review_texts = df['Review'].tolist()
+#             results = []
+#             for review in review_texts:
+#                 try:
+#                     with st.spinner(f'Analyzing review: {review}'):
+#                         sentiment, score = predict_sentiment(review)
+#                         score_percentage = [s * 100 for s in score]
+#                         results.append({"Review": review, "Sentiment": sentiment, "Positive Score": f"{score_percentage[1]:.2f}%", "Negative Score": f"{score_percentage[0]:.2f}%"})
+#                 except ValueError as e:
+#                     st.error(f"Error processing review '{review}': The review text must be a string. Please ensure all reviews are provided as text.")
+
+#                     continue
+            
+#             result_df = pd.DataFrame(results)
+#             st.write("Sentiment Analysis Results:")
+#             st.write(result_df)
+            
+#             result_df_melted = result_df.melt(id_vars=['Review', 'Sentiment'], value_vars=['Positive Score', 'Negative Score'], var_name='Type', value_name='Percentage')
+#             result_df_melted['Percentage'] = result_df_melted['Percentage'].str.rstrip('%').astype('float')
+#             result_df_melted['Review'] = result_df_melted['Review'].apply(shorten_text)
+            
+#             fig = px.bar(result_df_melted, x='Review', y='Percentage', color='Type', barmode='group', title="Sentiment Distribution")
+            
+#             fig.update_layout(
+#                 xaxis=dict(
+#                     tickangle=0  # Rotate the labels 45 degrees
+#                 )
+#             )
+#             st.plotly_chart(fig)
+
+
+
+
+# Main app content in the sidebar
+st.sidebar.header('📑 About This App')
 st.sidebar.write("""
-    Welcome to the Sentiment Analysis Web App! This application uses a powerful BERT model to classify the sentiment of user reviews as either positive or negative. Perfect for gaining insights into customer feedback.
-    
-    **Features:**
-    - ✨ Analyze individual reviews
-    - 🧐 Compare sentiments of multiple reviews
-    - 📊 Bulk review analysis through file upload
+    Welcome to the Sentiment Analysis Web App! Using a robust BERT model, this app classifies user reviews as positive, negative, or neutral, providing valuable insights into customer feedback. Enjoy exploring your data! 🎉
 """)
+
+# Sidebar content
+st.sidebar.header("💡 Example Reviews")
+
+# Example Reviews in one place
+st.sidebar.write("""
+    **Positive Reviews:**
+    - "This product exceeded my expectations! The quality is fantastic, and it's incredibly easy to use. Highly recommend it to anyone."
+    - "Absolutely love this item! It has made my life so much easier, and the customer service was outstanding."
+
+    **Negative Reviews:**
+    - "I'm very disappointed with this product. It broke after just two uses, and the quality is terrible. Would not recommend."
+    - "The worst purchase I've ever made. It didn't work as advertised, and the return process was a nightmare."
+
+    **Neutral Reviews:**
+    - "The product is okay. It does what it's supposed to, but nothing special. I might buy it again if it's on sale."
+    - "While the features are great, the battery life is quite short. It’s a bit of a trade-off."
+""")
+
